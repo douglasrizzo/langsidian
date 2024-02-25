ed3c97c9c92c72b43d9e06e9e89ced7eed5ad274
Douglas <douglasrizzom@gmail.com>
2024-02-25 16:29:50 -0300

    use gemma, ollama, nomic-embed

--- langsidian/__init__.py
+++ langsidian/__init__.py
@@ -1,24 +1,99 @@
+import logging
+import shutil
+from enum import Enum, auto
 from pathlib import Path
 
-import requests
-from tqdm import tqdm
+import re2
+from langchain.chains import RetrievalQA
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.embeddings.ollama import OllamaEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.text_splitter import MarkdownTextSplitter
+from langchain_community.document_loaders import ObsidianLoader, PyPDFDirectoryLoader
+from langchain_community.llms.ollama import Ollama
+from langchain_community.vectorstores.chroma import Chroma
 
 
-def download_with_progress(url: str | Path, filepath: Path) -> None:
-  """Download a file from the given URL to the specified filepath while displaying the download progress.
+class DocumentBase(Enum):
+  """List of document sources."""
 
-  From https://stackoverflow.com/a/37573701/1245214.
+  OBSIDIAN = auto()
+  PDF = auto()
 
-  Args:
-      url (str | Path): The URL of the file to download.
-      filepath (Path): The filepath to save the downloaded file.
-  """
-  # Sizes in bytes.
-  response = requests.get(url, stream=True)
-  total_size = int(response.headers.get("content-length", 0))
-  block_size = 1024
 
-  with tqdm(total=total_size, unit="B", unit_scale=True) as progress_bar, filepath.open("wb") as file:
-    for data in response.iter_content(block_size):
-      progress_bar.update(len(data))
-      file.write(data)
+class ChatBot:
+  """A Q&A chatbot class implemented using LangChain to answer questions while performing RAG on a document base."""
+
+  def __init__(
+    self,
+    docs_path: Path,
+    vectorstore_db_path: Path,
+    document_type: DocumentBase,
+    model_type: str,
+    embeddings: str = "nomic",
+  ) -> None:
+    """Initialize a chatbot object.
+
+    Args:
+        docs_path (Path): Path to directory containing the documents to be loaded.
+        vectorstore_db_path (Path): Path where the vector store will be persisted.
+        document_type (DocumentBase): Type of the document base.
+        model_type (str): Type of the model.
+        embeddings (str): Which embedding model to use. Defaults to "nomic".
+    """
+    logging.info("Loading documents")
+    if document_type == DocumentBase.OBSIDIAN:
+      docs = ObsidianLoader(docs_path, collect_metadata=True, encoding="UTF-8").load()
+      logging.info("Loaded %d documents", len(docs))
+      obsidian_hyperlink_pattern = r"\[\[([^\]]*?)\|([^\[]*?)\]\]"
+      for doc in docs:
+        s = re2.search(obsidian_hyperlink_pattern, doc.page_content)
+        if s is not None:
+          new_doc = re2.sub(obsidian_hyperlink_pattern, r"\2", doc.page_content)
+          doc.page_content = new_doc
+        doc.page_content = doc.page_content.replace("[[", "").replace("]]", "")
+    else:
+      docs = PyPDFDirectoryLoader(str(docs_path)).load()
+
+    logging.info("Loading chunks")
+    chunks = MarkdownTextSplitter(chunk_size=400, chunk_overlap=50).split_documents(docs)
+    logging.info("Loaded %d chunks", len(chunks))
+
+    logging.info("Clearing existing vector store persist directory")
+    shutil.rmtree(vectorstore_db_path, ignore_errors=True)
+    logging.info("Creating vector store")
+    vectordb = Chroma.from_documents(
+      documents=chunks,
+      embedding=HuggingFaceEmbeddings(model_kwargs={"device": "cpu"})
+      if embeddings == "huggingface"
+      else OllamaEmbeddings(model="nomic-embed-text"),
+      persist_directory=str(vectorstore_db_path),
+    )
+
+    logging.info("Loading LLM")
+    llm = Ollama(model=model_type)
+    logging.info("Loading prompt template")
+    with Path("templates", model_type.split(":")[0] + ".txt").open(encoding="utf-8") as f:
+      template = f.read()
+
+    qa_chain_prompt = PromptTemplate.from_template(template)
+    self.qa_chain = RetrievalQA.from_chain_type(
+      llm,
+      retriever=vectordb.as_retriever(search_type="mmr"),
+      return_source_documents=True,
+      chain_type_kwargs={"prompt": qa_chain_prompt},
+    )
+
+  def answer(self, query: str) -> str:
+    """Answer a query.
+
+    Args:
+        query (str): The query.
+
+    Returns:
+        str: The answer.
+    """
+    answer = self.qa_chain.invoke({"query": query})
+    for idx, document in enumerate(answer["source_documents"]):
+      logging.debug("Document %d: %s}", idx, document.page_content)
+    return answer["result"]
--- langsidian/__main__.py
+++ langsidian/__main__.py
@@ -1,115 +1,9 @@
 import argparse
 import logging
-import shutil
 from datetime import datetime
-from enum import Enum, auto
 from pathlib import Path
 
-import re2
-from langchain.chains import RetrievalQA
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
-from langchain.text_splitter import MarkdownTextSplitter
-from langchain_community.document_loaders import ObsidianLoader, PyPDFDirectoryLoader
-from langchain_community.llms.gpt4all import GPT4All
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain_community.vectorstores import Chroma
-
-from langsidian import download_with_progress
-
-
-class DocumentBase(Enum):
-  """List of document sources."""
-
-  OBSIDIAN = auto()
-  PDF = auto()
-
-
-class ModelType(Enum):
-  """List of available models."""
-
-  MISTRAL7B = auto()
-  GEMMA7B = auto()
-
-
-class ChatBot:
-  """A Q&A chatbot class implemented using LangChain to answer questions while performing RAG on a document base."""
-
-  def __init__(
-    self,
-    docs_path: Path,
-    vectorstore_db_path: Path,
-    document_type: DocumentBase,
-    model_type: ModelType,
-  ) -> None:
-    """Initialize a chatbot object.
-
-    Args:
-        docs_path (Path): Path to directory containing the documents to be loaded.
-        vectorstore_db_path (Path): Path where the vector store will be persisted.
-        document_type (DocumentBase): Type of the document base.
-        model_type (ModelType): Type of the model.
-    """
-    logging.info("Loading documents")
-    if document_type == DocumentBase.OBSIDIAN:
-      docs = ObsidianLoader(docs_path, collect_metadata=True, encoding="UTF-8").load()
-      logging.info("Loaded %d documents", len(docs))
-      obsidian_hyperlink_pattern = r"\[\[([^\]]*?)\|([^\[]*?)\]\]"
-      for doc in docs:
-        s = re2.search(obsidian_hyperlink_pattern, doc.page_content)
-        if s is not None:
-          new_doc = re2.sub(obsidian_hyperlink_pattern, r"\2", doc.page_content)
-          doc.page_content = new_doc
-        doc.page_content = doc.page_content.replace("[[", "").replace("]]", "")
-    else:
-      docs = PyPDFDirectoryLoader(str(docs_path)).load()
-    logging.info("Loading chunks")
-    chunks = MarkdownTextSplitter(chunk_size=400, chunk_overlap=50).split_documents(docs)
-    logging.info("Loaded %d chunks", len(chunks))
-
-    logging.info("Clearing existing vector store persist directory")
-    shutil.rmtree(vectorstore_db_path, ignore_errors=True)
-    logging.info("Creating vector store")
-    vectordb = Chroma.from_documents(
-      documents=chunks, embedding=HuggingFaceEmbeddings(), persist_directory=str(vectorstore_db_path)
-    )
-
-    logging.info("Loading LLM")
-    if model_type == ModelType.MISTRAL7B:
-      llm_weights_path = Path("models", "mistral-7b-openorca.Q4_0.gguf")
-      if not llm_weights_path.exists():
-        llm_weights_path.parent.mkdir(parents=True, exist_ok=True)
-        download_with_progress("https://gpt4all.io/models/gguf/mistral-7b-openorca.Q4_0.gguf", llm_weights_path)
-      llm = GPT4All(model=str(llm_weights_path), device="gpu")
-      with Path("templates/base.txt").open(encoding="utf-8") as f:
-        template = f.read()
-    else:
-      llm = HuggingFacePipeline.from_model_id(
-        model_id="google/gemma-7b-it",
-        task="text-generation",
-        # pipeline_kwargs={"max_new_tokens": 10},
-      )
-      with Path("templates/base.txt").open(encoding="utf-8") as f:
-        template = f.read()
-
-    qa_chain_prompt = PromptTemplate.from_template(template)
-    self.qa_chain = RetrievalQA.from_chain_type(
-      llm,
-      retriever=vectordb.as_retriever(search_type="mmr"),
-      return_source_documents=True,
-      chain_type_kwargs={"prompt": qa_chain_prompt},
-    )
-
-  def answer(self, query: str) -> str:
-    """Answer a query.
-
-    Args:
-        query (str): The query.
-
-    Returns:
-        str: The answer.
-    """
-    return self.qa_chain.invoke({"query": query})["result"]
+from langsidian import ChatBot, DocumentBase
 
 
 def main(args: argparse.Namespace) -> None:
@@ -118,11 +12,11 @@ def main(args: argparse.Namespace) -> None:
   Args:
       args (argparse.Namespace): Arguments from an argument parser.
   """
-  bot = ChatBot(args.docs_path, args.vectorstore_path, args.document_type, args.model_type)
+  bot = ChatBot(args.docs_path, args.vectorstore_path, args.document_type, args.ollama_model, args.embeddings)
 
   try:
     while True:
-      print(bot.answer(input("[Prompt]: ")))
+      print(f'[Assistant]: {bot.answer(input("[Prompt]: "))}')
   except KeyboardInterrupt:
     pass
 
@@ -149,13 +43,22 @@ if __name__ == "__main__":
     default=Path("docs/chroma/"),
   )
   parser.add_argument(
-    "--model_path",
-    type=Path,
-    help="Path to the model file.",
-    default=Path("models/my_little_llm.gguf"),
+    "--document_type", type=DocumentBase, help="The type of documents to load.", default=DocumentBase.OBSIDIAN
   )
   parser.add_argument(
-    "--document_type", type=DocumentBase, help="The type of documents to load.", default=DocumentBase.OBSIDIAN
+    "--ollama_model",
+    type=str,
+    help="Name of Ollama model to load.",
+    default="mistral:7b-instruct",
+    choices=["mistral:7b-instruct", "gemma:7b-instruct"],
+  )
+
+  parser.add_argument(
+    "--embeddings",
+    type=str,
+    help="The type of embedding to load.",
+    default="nomic",
+    choices=["nomic", "huggingface"],
   )
 
   main(parser.parse_args())
